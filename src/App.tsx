9a563e96bb7d34b54654150f4b9dc987144a11dc
Gonsix <gonsix3@gmail.com>
2025-09-02 14:57:30 +0900

    Setup: Tailwind css with Vite

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+      <h1 className="font-bold text-yellow-300">Tailwind CSSが効いてるよ</h1>
 
       <div className="row">
         <a href="https://vite.dev" target="_blank">
